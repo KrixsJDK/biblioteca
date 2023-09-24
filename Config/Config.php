af7cacc69cc1c9ee743e4646faf35b0940092452
KrixsJDK <caab.1315250025@gmail.com>
2023-09-24 15:07:51 -0500

    subir

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://3.143.246.247/biblioteca/";
+const host = "localhost";
+const user = "root";
+const pass = "152415";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
